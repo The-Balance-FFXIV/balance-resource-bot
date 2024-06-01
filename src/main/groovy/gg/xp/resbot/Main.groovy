fd3d94f72f76c0c3dd0f954f6e388ff71a9d2f46
XP <smurfs@smurfs.verymad.net>
2024-06-01 11:44:00 -0700

    Runnable build

--- src/main/groovy/gg/xp/resbot/Main.groovy
+++ src/main/groovy/gg/xp/resbot/Main.groovy
@@ -4,15 +4,17 @@ import groovy.transform.CompileStatic
 import reactor.util.Loggers
 
 @CompileStatic
-static void main(String[] args) {
-
-	def log = Loggers.getLogger(Main)
-	log.info 'Starting'
-	def bot = Bot.build {
-		tokenFromEnv()
-		dataDir('./data')
+class Main {
+	static void main(String[] args) {
+		def log = Loggers.getLogger(Main)
+		log.info 'Starting'
+		def bot = Bot.build {
+			tokenFromEnv()
+			dataDir('./data')
+		}
+		bot.start()
+		bot.runAll()
+		bot.stop()
 	}
-	bot.start()
-	bot.runAll()
-	bot.stop()
+
 }
